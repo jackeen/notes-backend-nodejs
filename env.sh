7451e08c03d8e4d2cbfe84870b81a276d56ce4e7
jack <sxzhangjia@gmail.com>
2022-11-21 23:25:31 +0800

    dev: adding test database name, default database name is changed

--- env.sh
+++ env.sh
@@ -1,8 +1,9 @@
 export DATABASE_HOST="localhost"
 export DATABASE_USER="postgres"
 export DATABASE_PASSWD=""
-export DATABASE_NAME="notes"
 export DATABASE_MAX_CONNECTION="6"
+export DATABASE_NAME="notes"
+export DATABASE_NAME_TEST="notes_test"
 
 export SERVER_PORT="8080"
 
